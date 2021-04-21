58cea16500a7d821df912b72c648b95c79f2a8ed
Avishy <avishy.menashe@gmail.com>
2021-04-21 18:05:53 +0300

    gradient change

---
+++ Assets/Scripts/GradientSpriteChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientSpriteChange : MonoBehaviour
+{
+    public Material GradientMat;
+    void Start()
+    {
+        GradientMat.SetColor("_Color", Color.black); // the left color
+        GradientMat.SetColor("_Color2", Color.white); // the right color
+    }
+}
